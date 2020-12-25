4936188aa78ae08e14992acfb15e6ab3a696fb62
abhilash1392 <abhilash1392@gmail.com>
2020-12-26 00:45:44 +0530

    Exercise 6

---
+++ exercise_6.py
@@ -0,0 +1,12 @@
+"""6. Write a Python program which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers."""
+
+x=map(str,input().split(','))
+
+y=tuple(x)
+z=list(y)
+print(z)
+print(y)
+
+
+
+
