f07151712aa3195e5e44baa18dc7cb683e729c6f
abhilash1392 <abhilash1392@gmail.com>
2020-12-26 00:10:47 +0530

    Exercise 5

---
+++ exercise_5.py
@@ -0,0 +1,10 @@
+"""5. Write a Python program which accepts the user's first and last name and print them in reverse order with a space between them."""
+
+first_name=input('Please enter your first name: ')
+
+last_name=input('Please enter your last name: ')
+
+print('Hello {} {}'.format(last_name,first_name))
+
+
+
