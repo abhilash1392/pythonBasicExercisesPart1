f92a852469e6acad5d96f0b484f743390c2b1937
abhilash1392 <abhilash1392@gmail.com>
2020-12-26 19:10:13 +0530

    Exercise 15

---
+++ exercise_15.py
@@ -0,0 +1,7 @@
+"""15. Write a Python program to get the volume of a sphere with radius 6.
+"""
+import math
+r=float(input("Enter the radius of the sphere: "))
+
+print('Volume of the sphere: {:.2f} unit cube.'.format(math.pi*(4/3)*(r**3)))
+
